b46c0650baa048d0b176cc067f34628847716b6a
Penlect <daniel.4ndersson@gmail.com>
2017-07-22 20:42:34 +0200

    Added matplotlib plot in test_pack.py

--- test/test_pack.py
+++ test/test_pack.py
@@ -1,14 +1,19 @@
 
+from collections import namedtuple
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.pyplot import cm
+import numpy as np
+
 import rpack as hw
+
+R = namedtuple('R', 'id width height x y')
 
 print(hw.helloworld())
 print()
 
-for a in dir(hw):
-    if a.startswith('buildvalue_'):
-        print('{:20}{!r:<20}{}'.format(a, getattr(hw, a)(), getattr(hw, a).__doc__))
-
-print(hw.pack([(1,3)]))
 test1 = [
     (12,32),
     (43,145),
@@ -19,4 +24,38 @@ test1 = [
 ]
 test1 = list(sorted(test1, key=lambda x: -x[-1]))
 print(test1)
-print(hw.pack(test1))
+
+test1 = [
+    (40,48),
+    (22,32),
+    (95,26),
+    (83,11)
+]
+
+
+result = [R(*[item[0], *item[1], *item[2]]) for item in hw.pack(test1)]
+
+width = max(r.width + r.x for r in result)
+height = max(r.height + r.y for r in result)
+print(width, height)
+norm = max(width, height)
+
+print(result)
+
+color=cm.rainbow(np.linspace(0.2, 0.8, len(result)))
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, aspect='equal')
+ax1.set_xlim([0,norm])
+ax1.set_ylim([0,norm])
+ax1.grid()
+for r, c in zip(result, color):
+    ax1.add_patch(
+        patches.Rectangle(
+            (r.x, r.y),   # (x,y)
+            r.width,          # width
+            r.height,          # height
+            facecolor=c
+        )
+    )
+plt.gca().invert_yaxis()
+plt.show()
